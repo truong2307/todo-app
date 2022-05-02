b9998d2c286d8e53d3221eb6bfd0f038e9930c5c
Vantruongg <toiiuvn013@gmail.com>
2022-05-02 13:04:42 +0700

    initial commit

---
+++ core.js
@@ -0,0 +1,36 @@
+export default function html ([first, ...string], ...value){
+    return value.reduce(
+        (acc, curr) => acc.concat(curr, string.shift())
+        , [first]
+    )
+    .filter(c => c && c !== true || c === 0)
+    .join('');
+}
+
+export function createStore(reducer){
+    let state = reducer();
+    const roots = new Map();
+
+    function render(){
+        for(const [root, component] of roots){
+            const output = component();
+            root.innerHTML = output; 
+        }
+    }
+
+    return {
+        attach(component, root){
+            roots.set(root, component)
+            render();
+        },
+        connect(selector = state => state){
+            return component => (props, ...args) =>
+                component(Object.assign({}, props, selector(state), ...args));
+
+        },
+        dispatch(action, ...args) {
+            state = reducer(state, action, args)
+            render();
+        }
+    }
+}
